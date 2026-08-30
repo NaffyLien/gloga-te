712a65e49de30ea23b5d91e3f5949ec91f9e7478
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-08-30 15:51:59 +0300

    test: implementing test and styles

--- src/main/java/pos/tarte/demo/controller/DanseurController.java
+++ src/main/java/pos/tarte/demo/controller/DanseurController.java
@@ -5,18 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import pos.tarte.demo.model.Danseur;
 import pos.tarte.demo.repository.DanseurRepository;
+import pos.tarte.demo.service.DanseurService;
 
 
 @Controller
 public class DanseurController {
     @Autowired
-    private DanseurRepository danseurRepository;
+    private DanseurService danseurService;
 
     @GetMapping("/danseurs/ajouter")
     public String formAjout(Model model) {
@@ -31,23 +29,21 @@ public class DanseurController {
             return "formulaire-danseur";
         }
 
-        danseurRepository.save(danseur);
+        danseurService.ajouter(danseur);
         return "redirect:/danseurs";
     }
 
     @GetMapping("/danseurs")
     public String listeDanseurs(Model model) {
-        model.addAttribute("danseurs", danseurRepository.findAll());
+        model.addAttribute("danseurs", danseurService.obtenirTous());
         return "liste-danseurs";
     }
 
     @GetMapping("/danseurs/modifier/{id}")
     public String formulaireModification(@PathVariable Long id, Model model) {
-        Danseur danseur = danseurRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("danseur introuvable : " + id));
-        model.addAttribute("danseur", danseur);
+        model.addAttribute("danseur", danseurService.obtenirParId(id));
         model.addAttribute("modeModification", true);
-        return "formulaire-danseur"; // on réutilise le même formulaire que pour l'ajout
+        return "formulaire-danseur";
     }
     @PostMapping("/danseurs/modifier/{id}")
     public String traiterModification(@PathVariable Long id,
@@ -56,14 +52,13 @@ public class DanseurController {
         if (result.hasErrors()) {
             return "formulaire-danseur";
         }
-        danseur.setId(id); // s'assure qu'on met à jour et non qu'on crée un nouvel enregistrement
-        danseurRepository.save(danseur);
+        danseurService.modifier(id, danseur);
         return "redirect:/danseurs";
     }
 
     @GetMapping("/danseurs/supprimer/{id}")
     public String supprimerdanseur(@PathVariable Long id) {
-        danseurRepository.deleteById(id);
+        danseurService.supprimer(id);
         return "redirect:/danseurs";
     }
 }
